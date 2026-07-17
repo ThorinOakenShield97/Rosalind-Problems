03784afba61dbef2c4a03b604b828fde1104a7b4
Eric Guillen Galvez <126527128+ThorinOakenShield97@users.noreply.github.com>
2026-07-17 14:13:19 +0200

    Add files via upload

---
+++ solutions/SPLC.py
@@ -0,0 +1,51 @@
+import sys
+
+codons = {
+    'UUU': 'F', 'UUC': 'F', 'UUA': 'L', 'UUG': 'L',
+    'UCU': 'S', 'UCC': 'S', 'UCA': 'S', 'UCG': 'S',
+    'UAU': 'Y', 'UAC': 'Y', 'UAA': 'Stop', 'UAG': 'Stop',
+    'UGU': 'C', 'UGC': 'C', 'UGA': 'Stop', 'UGG': 'W',
+    'CUU': 'L', 'CUC': 'L', 'CUA': 'L', 'CUG': 'L',
+    'CCU': 'P', 'CCC': 'P', 'CCA': 'P', 'CCG': 'P',
+    'CAU': 'H', 'CAC': 'H', 'CAA': 'Q', 'CAG': 'Q',
+    'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'CGG': 'R',
+    'AUU': 'I', 'AUC': 'I', 'AUA': 'I', 'AUG': 'M',
+    'ACU': 'T', 'ACC': 'T', 'ACA': 'T', 'ACG': 'T',
+    'AAU': 'N', 'AAC': 'N', 'AAA': 'K', 'AAG': 'K',
+    'AGU': 'S', 'AGC': 'S', 'AGA': 'R', 'AGG': 'R',
+    'GUU': 'V', 'GUC': 'V', 'GUA': 'V', 'GUG': 'V',
+    'GCU': 'A', 'GCC': 'A', 'GCA': 'A', 'GCG': 'A',
+    'GAU': 'D', 'GAC': 'D', 'GAA': 'E', 'GAG': 'E',
+    'GGU': 'G', 'GGC': 'G', 'GGA': 'G', 'GGG': 'G'
+}
+
+sequences = []
+s = ''
+for line in sys.stdin:
+    if line.startswith('>'):
+        ID = line[1:]
+        if len(s) != 0:
+            sequences.append(s)
+            s =''
+    else:
+        s += line.strip()
+    
+sequences.append(s)
+
+OG = sequences[0]
+introns = sequences[1:]
+
+for intron in introns:
+    OG = OG.replace(intron,'')
+    
+arn = OG.replace('T','U')
+
+protein =''
+for i in range(0,len(arn),3):
+    triplet = arn[i:i+3]
+    if codons[triplet] == 'Stop':
+        break
+    elif triplet in codons:
+        protein += codons[triplet]
+           
+print(protein)
